7811aefd7e64ef97d53c6aced44a17fd908134b6
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-06-16 13:27:37 +1000

    don't use mixin on forge for now

--- forge/src/main/java/koala/fishingreal/forge/FishingRealForge.java
+++ forge/src/main/java/koala/fishingreal/forge/FishingRealForge.java
@@ -2,7 +2,6 @@ package koala.fishingreal.forge;
 
 import koala.fishingreal.FishingReal;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.projectile.FishingHook;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.AddReloadListenerEvent;
@@ -21,22 +20,14 @@ public class FishingRealForge {
     }
 
     public static void onItemFished(ItemFishedEvent event) {
-        boolean useMixinInstead = event.getHookEntity().getClass() == FishingHook.class;
-        try {
-            useMixinInstead = event.getHookEntity().getClass().getMethod("m_37156_").getDeclaringClass() == FishingHook.class;
-        } catch (Exception ignored) {}
-
-        // Skip running when we know the mixin works. Still safe otherwise, just improves compat for other mixins.
-        if (!useMixinInstead) {
-            for (ItemStack itemStack : event.getDrops()) {
-                Entity convertedEntity = FishingReal.convertItemStack(itemStack, event.getEntity());
-                if (convertedEntity != null) {
-                    FishingReal.fishUpEntity(convertedEntity, event.getHookEntity(), itemStack, event.getEntity());
-                    if (!event.getEntity().isCreative()) {
-                        event.damageRodBy(1);
-                    }
-                    event.setCanceled(true);
+        for (ItemStack itemStack : event.getDrops()) {
+            Entity convertedEntity = FishingReal.convertItemStack(itemStack, event.getEntity());
+            if (convertedEntity != null) {
+                FishingReal.fishUpEntity(convertedEntity, event.getHookEntity(), itemStack, event.getEntity());
+                if (!event.getEntity().isCreative()) {
+                    event.damageRodBy(1);
                 }
+                event.setCanceled(true);
             }
         }
     }
